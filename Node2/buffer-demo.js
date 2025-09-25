5a1d0449a988e98e55cfacf24f19e14febb6abde
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-25 15:18:28 +0530

    first commit

---
+++ Node2/buffer-demo.js
@@ -0,0 +1,26 @@
+// objects -> hancle binary data
+// file system operations, cryptography , image processing
+
+
+const buffOne = Buffer.alloc(10);
+console.log(buffOne);
+
+
+// how to create buffer in string
+const bufferString = Buffer.from('Hello')
+console.log(bufferString)
+
+
+const bufferArrayInteger = Buffer.from([1, 2, 3, 4,, 5])
+console.log(bufferArrayInteger)
+
+buffOne.write("Node js")
+console.log('After writinfg Node js to bufferOne buffOne', buffOne.toString())
+
+console.log(bufferString[0])
+
+console.log(bufferString.slice(0, 3))
+
+
+const cancatBuffs = Buffer.concat([buffOne, bufferString])
+console.log(cancatBuffs)
