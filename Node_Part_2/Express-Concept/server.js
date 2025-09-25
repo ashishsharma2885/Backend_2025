3e11367c3a9e81ec46620864226a9f8b6b76fef6
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-25 16:44:11 +0530

    feat: implement CORS configuration for enhanced security

--- Node_Part_2/Express-Concept/server.js
+++ Node_Part_2/Express-Concept/server.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const {configureCors} = require('./config/cors.config')
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());         
+app.use(configureCors());         
 app.use(express.json()); 
 
 // Start server
