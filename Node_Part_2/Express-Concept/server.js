7f38f0614c4b089c228bfe892a139f0f33bb6353
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-25 20:17:26 +0530

    feat: add API versioning middleware and express-rate-limit dependency

--- Node_Part_2/Express-Concept/server.js
+++ Node_Part_2/Express-Concept/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {configureCors} = require('./config/cors.config')
 const { requestLogger, addTimeStamp } = require('./middlewares/customMiddleware')
 const {globalErrorhandler} = require('./middlewares/errorHandler')
+const { urlVersioning } = require('./middlewares/apiVersioning')
 const cors = require('cors');
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(addTimeStamp)
 // Middleware
 app.use(configureCors());         
 app.use(express.json()); 
+
+app.use('/api/v1', urlVersioning('v1'))
 
 app.use(globalErrorhandler)
 
