c187995f33db0292c0aeddabd0a339283aa84c42
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-26 16:19:17 +0530

    I am mastering Express.js and its middleware concepts, unlocking the power to build efficient, scalable, and organized web applications with clean and maintainable code

--- Node_Part_2/Express-Concept/server.js
+++ Node_Part_2/Express-Concept/server.js
@@ -5,6 +5,7 @@ const { requestLogger, addTimeStamp } = require('./middlewares/customMiddleware'
 const {globalErrorhandler} = require('./middlewares/errorHandler')
 const { urlVersioning } = require('./middlewares/apiVersioning')
 const {createBasicRateLimiter} = require('./middlewares/rateLimiting')
+const itemRoutes = require('./routes/item.routes')
 const cors = require('cors');
 
 const app = express();
@@ -16,10 +17,11 @@ app.use(addTimeStamp)
 
 // Middleware
 app.use(configureCors()); 
-app.use(createBasicRateLimiter(100, 15*60*100))   // 100 request  per 15 minitues    
+app.use(createBasicRateLimiter(2, 15*60*100))   // 100 request  per 15 minitues    
 app.use(express.json()); 
 
-app.use('/api/v1', urlVersioning('v1'))
+// app.use(urlVersioning('v1'))
+app.use('/api/v1', itemRoutes)
 
 app.use(globalErrorhandler)
 
